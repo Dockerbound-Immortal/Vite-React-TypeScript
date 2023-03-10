b569673389c43c62db594bbcf07122a083ce7fda
LukeMcCann <developer-lukemccann@outlook.com>
2023-03-10 21:30:28 +0000

    Resolve linter issues

--- services/cv-builder/client/src/test/App.test.tsx
+++ services/cv-builder/client/src/test/App.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from '../App/App';
 
 test('Renders the application.', () => {
-    render(<App />);
+	render(<App />);
 
-    const applicationContainer = screen.getByRole('application');
+	const applicationContainer = screen.getByRole('application');
 
-    expect(applicationContainer).toBeInTheDocument();
+	expect(applicationContainer).toBeInTheDocument();
 });
