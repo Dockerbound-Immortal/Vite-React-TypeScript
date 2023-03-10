3c4ed360c2cc296d4bdbbae5b70aba8b99094d89
LukeMcCann <developer-lukemccann@outlook.com>
2023-03-10 19:45:58 +0000

    Initial commit

---
+++ services/cv-builder/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    reactRefresh()
+  ],
+  server: {
+    host: process.env.LOCAL_HOST || '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 3000,
+    watch: {
+      usePolling: true,
+    }
+  },
+});
