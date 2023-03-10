603d4dbd51aa11751af9fa1fe1e606cd1f36d2ee
LukeMcCann <developer-lukemccann@outlook.com>
2023-03-10 21:29:23 +0000

    Update eslint rules

--- services/cv-builder/client/vite.config.ts
+++ services/cv-builder/client/vite.config.ts
@@ -1,29 +1,29 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactRefresh()
-  ],
-  server: {
-    host: process.env.LOCAL_HOST || '0.0.0.0',
-    port: parseInt(process.env.CLIENT_PORT, 10) || 3000,
-    watch: {
-      usePolling: true,
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    // CSS takes time to parse, only activate
-    // if you need tests to take CSS into consideration.
-    css: false
-  },
+	plugins: [
+		react(),
+		reactRefresh()
+	],
+	server: {
+		host: process.env.LOCAL_HOST || '0.0.0.0',
+		port: parseInt(process.env.CLIENT_PORT, 10) || 3000,
+		watch: {
+			usePolling: true,
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.ts',
+		// CSS takes time to parse, only activate
+		// If you need tests to take CSS into consideration.
+		css: false
+	},
 });
